d7ee64ffdc0eb2412936e3f02668de58115d73f0
Cesar Bernal <cesarbworkmail@gmail.com>
2019-02-02 09:31:46 +0000

    models and jdango panel, html

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# model is a class inside of the models module ????
+# creating a table name item where the objects will have to values name and done
+# all objets created in Item table will have this structure
+#the boolean will help us interact whit the item later on 
+class Item(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    done = models.BooleanField(blank=False, default=False)
+    def __str__(self):
+        return self.name
